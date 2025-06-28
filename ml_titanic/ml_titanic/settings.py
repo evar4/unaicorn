a99277a166da6cb9cb7bcaa05dba01ecb0b707ef
evar <varnai.emese@outlook.com>
2025-06-28 16:20:44 +0200

    added js and css files and static folder

--- ml_titanic/ml_titanic/settings.py
+++ ml_titanic/ml_titanic/settings.py
@@ -113,8 +113,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+import os.path
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join('static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
